f1c99bb3fcc0da37a453213b127c92920c34f1b3
Artudevp <abdielarturoart@gmail.com>
2024-10-24 19:56:16 -0500

    Se arregla error de CORS permitiendo recibir peticiones

---
+++ src/main/java/com/ecotourexpress/ecotourexpress/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ecotourexpress.ecotourexpress.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite CORS para todas las rutas
+                .allowedOrigins("http://localhost:3000") // Cambia esto por tu origen permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Permite credenciales (cookies, auth headers, etc.)
+    }
+}
