b7f6690b39bc2967d0c33b3e9c06b74927a2b553
Artudevp <abdielarturoart@gmail.com>
2025-02-09 17:59:22 -0500

    Cambio de empaquetaodr de la clase principal de la aplicación war -> jar

--- src/main/java/com/ecotourexpress/ecotourexpress/EcoTourExpressApplication.java
+++ src/main/java/com/ecotourexpress/ecotourexpress/EcoTourExpressApplication.java
@@ -2,12 +2,10 @@ package com.ecotourexpress.ecotourexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
-public class EcotourexpressApplication {
+public class EcoTourExpressApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EcotourexpressApplication.class, args);
+        SpringApplication.run(EcoTourExpressApplication.class, args);
     }
 }
